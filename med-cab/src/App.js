605d039f140bd1e5844d4fe199cc77d172d8ac69
Frances Hansen <francesjuniperhansen@gmail.com>
2020-03-02 14:50:09 -0800

    signup form ready for merge

--- med-cab/src/App.js
+++ med-cab/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage} from 'formik'
-import Signup from './components/Signup'
-import Signin from './components/Signin'
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Signin/>
-      <Signup/>
+      
+      
     </div>
   );
 }
